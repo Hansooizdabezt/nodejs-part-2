f18895bb0ea5cb016abfaca21b353f6713796808
hansoo280102 <tantran012080@gmail.com>
2025-02-21 21:29:38 +0700

    81

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -1,11 +1,67 @@
 const User = require('../models/user');
 
 const getAllUsers = async (req, res) => {
-    let users = await User.find();
-    return res.status(200).json({
-        data: users,
-        message: 'Get all users successfully'
-    });
+    try {
+        let users = await User.find();
+        return res.status(200).json({
+            message: 'Get all users successfully',
+            data: users
+        });
+    } catch (error) {
+        console.log("Error Get All User: " + error);
+        res.status(500).json({
+            message: 'Error getting all users'
+        });
+    }
+
 }
 
-module.exports = { getAllUsers }
+const createUser = async (req, res) => {
+    try {
+        let { email, name, city } = req.body;
+
+        const newUser = await User.create({ email, name, city });
+        return res.status(201).json({
+            message: 'Create user successfully',
+            data: newUser
+        });
+    } catch (error) {
+        console.log("Error Create User: " + error);
+        res.status(500).json({
+            message: 'Error creating user'
+        });
+    }
+}
+
+const updateUser = async (req, res) => {
+    try {
+        let { email, name, city, userId } = req.body;
+
+        await User.updateOne({ _id: userId }, { email, name, city });
+        return res.status(200).json({
+            message: 'Update user successfully'
+        });
+    } catch (error) {
+        console.log("Error Update User: " + error);
+        res.status(500).json({
+            message: 'Error updating user'
+        });
+    }
+}
+
+const deleteUser = async (req, res) => {
+    try {
+        let { userId } = req.body;
+        await User.deleteOne({ _id: userId });
+        return res.status(200).json({
+            message: 'Delete user successfully'
+        });
+    } catch (error) {
+        console.log("Error Delete User: " + error);
+        res.status(500).json({
+            message: 'Error deleting user'
+        });
+    }
+}
+
+module.exports = { getAllUsers, createUser, updateUser, deleteUser }
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,24 +1,16 @@
 const express = require('express')
-const { getAllUsers } = require('../controllers/apiController')
+const { getAllUsers, createUser, updateUser, deleteUser } = require('../controllers/apiController')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send("this is a test api request")
-})
-
-router.get('/201', (req, res) => {
-    res.status(201).json({
-        data: {
-            id: 1,
-            name: "John Doe"
-        },
-        message: "this is a 201 response"
-
-    })
-})
-
 router.get('/users', getAllUsers);
+
+router.post('/users', createUser);
+
+router.put('/users', updateUser);
+
+router.delete('/users', deleteUser);
+
 
 
 
